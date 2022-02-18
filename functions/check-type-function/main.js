15f801761ebda2ab711f67d13a7d317532befa49
savami <sava.v.miljkovic@gmail.com>
2022-02-18 17:51:19 +0100

    completed tasks check-type-function and join-array-members

---
+++ functions/check-type-function/main.js
@@ -0,0 +1,20 @@
+var fox = ['The', 'quick', 'brown', 'fox', 'jumps', 'over', 'the', 'lazy', 'dog'];
+
+var obj = {
+    name: "Sava",
+    lastname: "Miljkovic",
+    age: 26,
+    gender: "male"
+}
+
+var helloWorld = 'Hello World!'
+
+var zero = null;
+
+var nothing = undefined;
+
+function checkArray(variableName) {
+    console.log(Array.isArray(variableName))
+}
+
+checkArray()
