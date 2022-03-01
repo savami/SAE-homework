ab32e21d5b3935f555d3a855ee9d496112d00bdc
savami <sava.v.miljkovic@gmail.com>
2022-03-01 09:53:11 +0100

    fixed global variable main.js

--- functions/global-variable/main.js
+++ functions/global-variable/main.js
@@ -1,18 +1,11 @@
 var promptValue = [prompt('Enter a value'), prompt('Enter a second value'), prompt('Enter a third value')]
 
+console.log(promptValue)
 
 function printArray() {
-
     var promptPrint = document.querySelector('#promptPrint')
     
     promptPrint.textContent = promptValue.join(' ')
 
 }
 
-// promptElement.addEventListener("click", function(){
-//     for(var i = 1; i < promptValue; i++) {
-//       promptElement.innerHTML = promptValue;
-//     }
-// })
-
-console.log(promptValue)
