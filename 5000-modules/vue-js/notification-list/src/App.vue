77fd7f6a08065b5fa42b55a77b5c90fac154e65f
Sava Miljkovic <sava@MacBookPro-M1-S.local>
2022-06-07 23:50:02 +0200

    completed faded readstatus & display date

---
+++ 5000-modules/vue-js/notification-list/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <NotificationList/>
+  </div>
+</template>
+
+<script>
+import NotificationList from './components/NotificationList.vue'
+
+export default {
+  name: 'App',
+  components: {
+    NotificationList
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
