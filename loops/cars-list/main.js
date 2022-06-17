17780f08d28ba846934e5ae6f7203cca3c29f134
savami <sava.v.miljkovic@gmail.com>
2022-06-17 19:48:33 +0200

    deleted double folders

--- loops/cars-list/main.js
+++
@@ -1,9 +0,0 @@
-var cars = ['Renault', 'Peugeot', 'Mazda', 'Mercedes', 'Yugo', 'Honda']
-
-function printArray() {
-    cars.forEach(function(el){
-      document.getElementById('result').innerHTML+='<br>' + el;
-    })
-  }
-  
-printArray()
